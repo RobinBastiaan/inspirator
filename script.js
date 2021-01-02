e47cc9680997e322079aadfa456262ca5aff916d
RobinBastiaan <robinbastiaan@gmail.com>
2021-01-02 12:38:36 +0100

    :sparkles: Let script use new input table as source

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let games = [];
 let when = 0;
 
 class programClass {
-    constructor(name, type, location, date, special) {
+    constructor(name, location, type, date, special) {
         this.name = name;
-        this.type = type;
         this.location = location;
+        this.type = type;
         this.date = date;
         this.special = special;
     }
@@ -31,44 +31,42 @@ function getProgram() {
         programArray.push(program);
     }
 
-    // TODO randomize result
-
     return programArray;
 }
 
 //<script>/*1*/// display the programs with their values
 function showPrograms(games, found, page) {
     for (let i = 0; i < itemsPerPage; i++) {
-        if (games[i + ((page - 1) * itemsPerPage)][4] === "Rood") {
+        if (games[i + ((page - 1) * itemsPerPage)]['date'] === "Rood") {
             document.getElementById(i + 1 + "eName").innerHTML =
-                "<i class='red'>" + ((i + 1) + ((page - 1) * itemsPerPage)) + ". " + games[i + ((page - 1) * itemsPerPage)][0] + "</i>";
+                "<i class='red'>" + ((i + 1) + ((page - 1) * itemsPerPage)) + ". " + games[i + ((page - 1) * itemsPerPage)]['name'] + "</i>";
         } else {
             document.getElementById(i + 1 + "eName").innerHTML =
-                ((i + 1) + ((page - 1) * itemsPerPage)) + ". " + games[i + ((page - 1) * itemsPerPage)][0];
+                ((i + 1) + ((page - 1) * itemsPerPage)) + ". " + games[i + ((page - 1) * itemsPerPage)]['name'];
         }
         document.getElementById(i + 1 + "eType").innerHTML =
-            (games[i + ((page - 1) * itemsPerPage)][1] === "Uitdagende Scoutingtechnieken") ? "Scoutingtechnieken" : games[i + ((page - 1) * itemsPerPage)][1];
-        if (games[i + ((page - 1) * itemsPerPage)][2] !== "") {
-            switch (games[i + ((page - 1) * itemsPerPage)][2]) {
+            (games[i + ((page - 1) * itemsPerPage)]['type'] === "Uitdagende Scoutingtechnieken") ? "Scoutingtechnieken" : games[i + ((page - 1) * itemsPerPage)]['type'];
+        if (games[i + ((page - 1) * itemsPerPage)]['location'] !== "") {
+            switch (games[i + ((page - 1) * itemsPerPage)]['location']) {
                 case "Bos":
                     document.getElementById(i + 1 + "eLoca").innerHTML =
-                        "<img src='http://franciscus.pbworks.com/f/bos.png' alt='' title='Bos' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)][2];
+                        "<img src='http://franciscus.pbworks.com/f/bos.png' alt='' title='Bos' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)]['location'];
                     break;
                 case "Veld":
                     document.getElementById(i + 1 + "eLoca").innerHTML =
-                        "<img src='http://franciscus.pbworks.com/f/veld.png' alt='' title='Veld' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)][2];
+                        "<img src='http://franciscus.pbworks.com/f/veld.png' alt='' title='Veld' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)]['location'];
                     break;
                 case "Clubhuis":
                     document.getElementById(i + 1 + "eLoca").innerHTML =
-                        "<img src='http://franciscus.pbworks.com/f/clubhuis.png' alt='' title='Clubhuis' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)][2];
+                        "<img src='http://franciscus.pbworks.com/f/clubhuis.png' alt='' title='Clubhuis' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)]['location'];
                     break;
                 case "Binnenstad":
                     document.getElementById(i + 1 + "eLoca").innerHTML =
-                        "<img src='http://franciscus.pbworks.com/f/binnenstad.png' alt='' title='Binnenstad' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)][2];
+                        "<img src='http://franciscus.pbworks.com/f/binnenstad.png' alt='' title='Binnenstad' height='15' width='15'> " + games[i + ((page - 1) * itemsPerPage)]['location'];
                     break;
                 default:
                     document.getElementById(i + 1 + "eLoca").innerHTML =
-                        "<img src='http://franciscus.pbworks.com/f/locatie.png' alt='' title='Locatie' height='15' width='20'> " + games[i + ((page - 1) * itemsPerPage)][2];
+                        "<img src='http://franciscus.pbworks.com/f/locatie.png' alt='' title='Locatie' height='15' width='20'> " + games[i + ((page - 1) * itemsPerPage)]['location'];
             }
         } else { // no program = empty
             document.getElementById(i + 1 + "eLoca").innerHTML = "";
@@ -80,7 +78,7 @@ function showPrograms(games, found, page) {
             if (games[i + ((page - 1) * itemsPerPage)][3] === "0/0") {
                 eWhen += "-- / ----";
             } else {
-                eWhen += games[i + ((page - 1) * itemsPerPage)][3];
+                eWhen += games[i + ((page - 1) * itemsPerPage)]['date'];
             }
         }
         document.getElementById(i + 1 + "eWhen").innerHTML = eWhen;
@@ -88,14 +86,14 @@ function showPrograms(games, found, page) {
 
     document.getElementById("found").innerHTML = found; // set #results found
 
-    for (let j = 1; j <= 10; j++) { // max 10 pagemarkers
+    for (let j = 1; j <= 10; j++) { // max 10 page markers
         if (found - ((j - 1) * itemsPerPage) > 0) { // if that page has programs
             if (j === page) { // to set and reset the boldness of the current page
                 document.getElementById(j.toString()).innerHTML = "<b><u>[" + j + "]</u></b>";
             } else {
                 document.getElementById(j.toString()).innerHTML = "<span>[" + j + "]</span>";
             }
-        } else { // hide the pagemarker if the page is empty
+        } else { // hide the page marker if the page is empty
             document.getElementById(j.toString()).innerHTML = " "; // invisible char for vertical spacing(alt+255)
         }
     }
@@ -113,7 +111,7 @@ function showValue(newValue) {
     }
 }
 
-// select all checkboxes with the same name (type/loca)
+// select all checkboxes with the same name (type/location)
 function checkboxToggle(source, name) {
     let checkboxes = document.getElementsByName(name);
     for (let i = 0, n = checkboxes.length; i < n; i++) {
@@ -126,45 +124,45 @@ function checkboxToggle(source, name) {
 //<script>/*2*/// search for the programs
 function search(page) {
     // new type-input
-    let type = [];
+    let selectedTypes = [];
     if (document.querySelector('input[value="creatief"]').checked) {
-        type.push("Creatief");
+        selectedTypes.push("Creatief");
     }
     if (document.querySelector('input[value="sport"]').checked) {
-        type.push("Sport & Spel");
+        selectedTypes.push("Sport & Spel");
     }
     if (document.querySelector('input[value="strategisch"]').checked) {
-        type.push("Strategisch");
+        selectedTypes.push("Strategisch");
     }
     if (document.querySelector('input[value="toneel"]').checked) {
-        type.push("Toneel");
+        selectedTypes.push("Toneel");
     }
     if (document.querySelector('input[value="scoutingtechinieken"]').checked) {
-        type.push("Uitdagende Scoutingtechnieken");
+        selectedTypes.push("Uitdagende Scoutingtechnieken");
     }
     if (document.querySelector('input[value="veilig"]').checked) {
-        type.push("Veilig & Gezond");
+        selectedTypes.push("Veilig & Gezond");
     }
     if (document.querySelector('input[value="thema"]').checked) {
-        type.push("Thema");
+        selectedTypes.push("Thema");
     }
     if (document.querySelector('input[value="korte"]').checked) {
-        type.push("Korte spelletjes");
+        selectedTypes.push("Korte spelletjes");
     }
 
     // new loca-input
-    let loca = [];
+    let selectedLocations = [];
     if (document.querySelector('input[value="bos"]').checked) {
-        loca.push("Bos");
+        selectedLocations.push("Bos");
     }
     if (document.querySelector('input[value="veld"]').checked) {
-        loca.push("Veld");
+        selectedLocations.push("Veld");
     }
     if (document.querySelector('input[value="clubhuis"]').checked) {
-        loca.push("Clubhuis");
+        selectedLocations.push("Clubhuis");
     }
     if (document.querySelector('input[value="binnenstad"]').checked) {
-        loca.push("Binnenstad");
+        selectedLocations.push("Binnenstad");
     }
 
     // new special option-input
@@ -187,56 +185,56 @@ function search(page) {
     games = []; // reset game values
 
     // get programs
-    let programArray = getProgram();
+    let programs = getProgram();
 
-    for (let i = 0; i < programs.length; i++) { // cycle through all prog
-        for (let j = 0; j < type.length; j++) { // cycle through prog-type checked
-            for (let k = 0; k < loca.length; k++) { // cycle through prog-loca checked
-                if (valueInArray(programs[i][1], type[j])) { // check if the prog has the right type
-                    if (valueInArray(programs[i][2], loca[k])) { // check if the prog has the right loca
-                        if (never === true && ever === true) { // check if user wants to see ever&never done prog's
-                            if (red === true) { // check if user wants to include red prog's
-                                if (calculateDifference(programs[i][3]) >= when) { // check if the prog has right date
-                                    games.push(eval("programs[" + i + "]")); // all prog's
-                                    rank++;
-                                    found++;
-                                }
-                            } else { // all not-red prog's and right date
-                                if ((programs[i][4] !== "Rood") && (calculateDifference(programs[i][3]) >= when)) { // check if the prog is not red and right date
-                                    games.push(eval("programs[" + i + "]"));
-                                    rank++;
-                                    found++;
-                                }
-                            }
-                        } else if (never === true) { // check if user wants to ONLY see never done prog's
-                            if ((programs[i][3] === "0/0") && (programs[i][4] !== "Rood")) { // check if the prog has never been done
-                                games.push(eval("programs[" + i + "]"));
-                                rank++;
-                                found++;
-                            }
-                            if (red === true) { // check if user wants to include red prog's
-                                if ((programs[i][3] === "0/0") && (programs[i][4] === "Rood")) { // check if the prog has never been done and is red
-                                    games.push(eval("programs[" + i + "]"));
-                                    rank++;
-                                    found++;
-                                }
-                            }
-                        } else if (ever === true) { // check if user wants to ONLY see ever done prog's
-                            if ((programs[i][3] !== "0/0") && (calculateDifference(programs[i][3]) >= when)) { // check if the prog has ever been done and right date
-                                games.push(eval("programs[" + i + "]"));
-                                rank++;
-                                found++;
-                            } // no need to check for red here; asume done implies not red
-                        }
-                    }
+    let programsLength = programs.length;
+    for (let i = 0; i < programsLength; i++) { // cycle through all programs and only add those that match the search parameters
+        if (!selectedTypes.includes(programs[i]['type'])) {
+            continue;
+        }
+        if (!selectedLocations.includes(programs[i]['location'])) {
+            continue;
+        }
+
+        if (never === true && ever === true) { // check if user wants to see ever&never done programs
+            if (red === true) { // check if user wants to include red programs
+                if (calculateDifference(programs[i]['date']) >= when) { // check if the program has right date
+                    games.push(eval("programs[" + i + "]")); // all programs
+                    rank++;
+                    found++;
+                }
+            } else { // all not-red prog's and right date
+                if ((programs[i]['date'] !== "Rood") && (calculateDifference(programs[i]['date']) >= when)) { // check if the program is not red and right date
+                    games.push(eval("programs[" + i + "]"));
+                    rank++;
+                    found++;
                 }
             }
+        } else if (never === true) { // check if user wants to ONLY see never done programs
+            if ((programs[i]['date'] === "0/0") && (programs[i]['date'] !== "Rood")) { // check if the program has never been done
+                games.push(eval("programs[" + i + "]"));
+                rank++;
+                found++;
+            }
+            if (red === true) { // check if user wants to include red programs
+                if ((programs[i]['date'] === "0/0") && (programs[i]['date'] === "Rood")) { // check if the program has never been done and is red
+                    games.push(eval("programs[" + i + "]"));
+                    rank++;
+                    found++;
+                }
+            }
+        } else if (ever === true) { // check if user wants to ONLY see ever done programs
+            if ((programs[i]['date'] !== "0/0") && (calculateDifference(programs[i]['date']) >= when)) { // check if the program has ever been done and right date
+                games.push(eval("programs[" + i + "]"));
+                rank++;
+                found++;
+            } // no need to check for red here; assume done implies not red
         }
     }
 
     // sort the results on date
     games.sort(function (a, b) {
-        return parseFloat(calculateDifference(b[3])) - parseFloat(calculateDifference(a[3]))
+        return parseFloat(calculateDifference(b['date'])) - parseFloat(calculateDifference(a['date']))
     });
 
     // make sure there are empty results so there will be a correct output
@@ -245,6 +243,8 @@ function search(page) {
         rank++;
     }
 
+    console.log(games);
+
     // show the result
     showPrograms(games, found, page);
 }
@@ -252,19 +252,20 @@ function search(page) {
 //</script>
 
 //<script>/*3*/// check if a values is in an array
-function valueInArray(arr, obj) {
-    return (arr.indexOf(obj) !== -1);
-}
 
 // calculate difference in months with current time
-function calculateDifference(program_date) {
+function calculateDifference(programDate) {
+    if (programDate === '*') { // program can always be done
+        return 0;
+    }
+
     // get today value
     let today = new Date();
     let mm = today.getMonth() + 1; //January is 0!
     let yyyy = today.getFullYear();
 
-    let when_res = program_date.split("/");
-    return ((yyyy - when_res[1]) * 12) + (mm - when_res[0]); // calculate difference in months
+    let whenResult = programDate.split("/");
+    return ((yyyy - whenResult[1]) * 12) + (mm - whenResult[0]); // calculate difference in months
 }
 
 // initial function calls
